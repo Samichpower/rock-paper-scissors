115e9bdfb002c9420f830f0ccda76d38521ce830
Austin <samichpower@yahoo.com>
2022-08-20 07:22:31 -0500

    Create playRound function to determine the winner of a round of Rock Paper Scissors

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ function getComputerChoice() {
   }
 }
 
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock" && computerSelection == "scissors") {
+    return "You win! Rock beats Scissors";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "You lose! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "You win! Paper beats Rock";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "You lose! Scissors beats Paper";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "You win! Scissors beats Paper!";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "You lose! Rock beats Scissors!";
+  } else if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  } else {
+    return "Try again!";
+  }
+}
+
+
+let playerSelection = prompt("Rock, Paper, or Scissors?");
+playerSelection = playerSelection.toLowerCase();
+
+let computerSelection = getComputerChoice()
+
+console.log(`You chose ${playerSelection}, your opponent picked ${computerSelection}.`);
+
+console.log(playRound(playerSelection, computerSelection));
