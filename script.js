7bf2efa47d4b11b55816e11f55045cf52fe9cd92
Austin <samichpower@yahoo.com>
2022-08-20 07:05:42 -0500

    Make getComputerChoice answers lowercase

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ function getComputerChoice() {
   let randomInt = Math.floor(Math.random() * 3) + 1;
 
   if (randomInt === 1) {
-    return "Rock";
+    return "rock";
   } else if (randomInt === 2) {
-    return "Paper";
+    return "paper";
   } else if (randomInt === 3) {
-    return "Scissors";
+    return "scissors";
   }
 }
 
-console.log(getComputerChoice());
